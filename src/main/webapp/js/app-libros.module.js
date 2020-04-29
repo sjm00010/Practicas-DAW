0b416a1f33c853f56ae15c428f8d308c0c76435c
Sergio Jiménez Moreno <sjm00010@red.ujaen.es>
2020-04-29 11:36:47 +0200

    Ej 4 listo

--- src/main/webapp/js/app-libros.module.js
+++ src/main/webapp/js/app-libros.module.js
@@ -4,25 +4,40 @@ class LibrosController {
 
         this.$http = $http;
         this.serviceUrl = 'api/libros';
-        
+
         //init view-model
         this.visualiza = true;
-        this.libros=[];
+        this.editando = false;
+        this.libros = [];
         this.cargaLibros();
         this.libro = {ISBN: "", titulo: "", fecha: "XXXX", precio: 0};
     }
 
     altaLibro() {
-        this.$http.post(this.serviceUrl, this.libro)
-                .then(response => {
-                    console.log("Nuevo libro creado");
-                    this.libro = {};
-                    this.visualiza = false;
-                    this.errorMsgs = [];
-                }).then(() => this.cargaLibros()
-        ).catch(response => { //bean validation errors
-            this.errorMsgs = response.data;
-        });
+        if (this.editando) {
+            this.$http.put(this.serviceUrl + '/' + this.libro.ISBN, this.libro)
+                    .then(response => {
+                        console.log("Libro editado");
+                        this.libro = {};
+                        this.visualiza = false;
+                        this.editando = false;
+                        this.errorMsgs = [];
+                    }).then(() => this.cargaLibros()
+            ).catch(response => { //bean validation errors
+                this.errorMsgs = response.data;
+            });
+        } else {
+            this.$http.post(this.serviceUrl, this.libro)
+                    .then(response => {
+                        console.log("Nuevo libro creado");
+                        this.libro = {};
+                        this.visualiza = false;
+                        this.errorMsgs = [];
+                    }).then(() => this.cargaLibros()
+            ).catch(response => { //bean validation errors
+                this.errorMsgs = response.data;
+            });
+        }
     }
 
     cargaLibros() {
@@ -33,7 +48,7 @@ class LibrosController {
             this.libros = response.data;
         });
     }
-    
+
     borraLibro(isbn) {
         this.$http.delete(this.serviceUrl + '/' + isbn)
                 .then(response => {
@@ -44,9 +59,22 @@ class LibrosController {
             this.errorMsgs = response.data;
         });
     }
-    
-    visualizaForm(){
+
+    visualizaForm() {
         this.visualiza = true;
+    }
+
+    editar(isbn) {
+        this.editando = true;
+        this.$http.get(this.serviceUrl + '/' + isbn)
+                .then(response => {
+                    console.log("Editar libro");
+                    this.libro = response.data;
+                    this.errorMsgs = [];
+                }).then(() => this.visualizaForm()
+        ).catch(response => { //bean validation errors
+            this.errorMsgs = response.data;
+        });
     }
 }
 
