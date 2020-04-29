3d12f97f08bc8e3bd3764027be041acdc7f32882
Sergio Jiménez Moreno <sjm00010@red.ujaen.es>
2020-04-29 11:09:11 +0200

    Ej 2 y 3 listo

--- src/main/webapp/js/app-libros.module.js
+++ src/main/webapp/js/app-libros.module.js
@@ -4,17 +4,20 @@ class LibrosController {
 
         this.$http = $http;
         this.serviceUrl = 'api/libros';
+        
         //init view-model
+        this.visualiza = true;
+        this.libros=[];
         this.cargaLibros();
         this.libro = {ISBN: "", titulo: "", fecha: "XXXX", precio: 0};
     }
 
     altaLibro() {
-        this.libros.push(this.libro);
         this.$http.post(this.serviceUrl, this.libro)
                 .then(response => {
-                    console.log("Libro enviado");
+                    console.log("Nuevo libro creado");
                     this.libro = {};
+                    this.visualiza = false;
                     this.errorMsgs = [];
                 }).then(() => this.cargaLibros()
         ).catch(response => { //bean validation errors
@@ -29,6 +32,21 @@ class LibrosController {
         }).then(response => {
             this.libros = response.data;
         });
+    }
+    
+    borraLibro(isbn) {
+        this.$http.delete(this.serviceUrl + '/' + isbn)
+                .then(response => {
+                    console.log("Libro borrado");
+                    this.errorMsgs = [];
+                }).then(() => this.cargaLibros()
+        ).catch(response => { //bean validation errors
+            this.errorMsgs = response.data;
+        });
+    }
+    
+    visualizaForm(){
+        this.visualiza = true;
     }
 }
 
